a4971c69218e03bc2cd3302a069992c567a045a3
BK201-sys <58057781+BK201-sys@users.noreply.github.com>
2020-01-12 16:43:14 +0800

    Update Forms.py

--- Forms.py
+++ Forms.py
@@ -3,14 +3,17 @@ from wtforms import Form, StringField, SelectField, validators, FloatField, Inte
 
 class importForm(Form):
 
-    def validate_float(form, field):
-        if field.data <= 0:
-            raise ValidationError("Price must be a valid value!")
-
-    brand = SelectField("Brand Name", [validators.DataRequired()], choices=[("","Select"), ("pilot","Pilot"), ("uni-ball", "Uni-ball")])
+    brand = SelectField("Brand Name", [validators.DataRequired()], choices=[("","Select"), ("pilot","Pilot"), ("uni-ball", "Uni-ball")], default="")
     productName = StringField("Product Name", [validators.DataRequired(), validators.Length(min=1, max=70)])
     quantity = IntegerField("Quantity", [validators.DataRequired(), validators.NumberRange(min=1, max=200)])
     costPrice = FloatField("Cost Price", [validators.DataRequired(), validators.NumberRange(min=0.01)])
     sellPrice = FloatField("Selling Price", [validators.DataRequired(), validators.NumberRange(min=0.01)])
 
 
+class updateForm(Form):
+
+    productName = StringField("Product Name", [validators.DataRequired(), validators.Length(min=1, max=70)])
+    currentStock = IntegerField("Current Stock", [validators.DataRequired()])
+    soldStock = IntegerField("Sold Stock", [validators.DataRequired()])
+    costPrice = FloatField("Cost Price", [validators.DataRequired(), validators.NumberRange(min=0.01)])
+    sellPrice = FloatField("Selling Price", [validators.DataRequired(), validators.NumberRange(min=0.01)])
