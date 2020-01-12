396de66701bb29db2edadf4ccddb42f7f41b8e77
BK201-sys <58057781+BK201-sys@users.noreply.github.com>
2020-01-12 16:45:47 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-import shelve, cookie, inventory
+import shelve, cookie, Forms
+import inventory as inv
 from Forms import importForm
 
 app = Flask(__name__, static_folder="static")
@@ -27,25 +28,67 @@ def inventory():
     for keys in invDict:
         good = invDict.get(keys)
         stockList.append(good)
-    print(stockList)
     return render_template("inventory.html", cookieDict=cookieDict, stockList=stockList, count=len(invDict))
 
 
 @app.route("/importForm", methods=["GET", "POST"])
 def importFormPage():
-    ImportForm = importForm(request.form)
+    ImportForm = Forms.importForm(request.form)
 
     if request.method == "POST" and ImportForm.validate():
         invDict = {}
         db = shelve.open("inventory.db", "c")
         invDict = db["inventory"]
-        name = str(ImportForm.brand.data + " " + ImportForm.productName.data).split()
-        name = " ".join(name).capitalize()
-        good = inventory.Inventory(name, ImportForm.quantity.data, ImportForm.costPrice.data, ImportForm.sellPrice.data)
+        good = inv.Inventory(ImportForm.brand.data, ImportForm.productName.data, ImportForm.quantity.data, ImportForm.costPrice.data, ImportForm.sellPrice.data)
         invDict[good.get_stockID()] = good
         db["inventory"] = invDict
+        db.close()
         return redirect(url_for("inventory"))
     return render_template("importGoods.html", cookieDict=cookieDict, form=ImportForm)
+
+
+@app.route("/updateStockInfo/<int:id>", methods=["GET", "POST"])
+def updateStockInfo(id):
+    updateStockInfoForm = Forms.updateForm(request.form)
+
+    if request.method == "POST" and updateStockInfoForm.validate():
+        invDict = {}
+        db = shelve.open("inventory.db", "w")
+        invDict = db["inventory"]
+        stock = invDict.get(id)
+        brand = stock.get_name().split(" ", 1)[0]
+        stock.set_name(brand, updateStockInfoForm.productName.data)
+        stock.set_currentStock(updateStockInfoForm.currentStock.data)
+        stock.set_soldStock(updateStockInfoForm.soldStock.data)
+        stock.set_costPrice(updateStockInfoForm.costPrice.data)
+        stock.set_sellingPrice(updateStockInfoForm.sellPrice.data)
+        db["inventory"] = invDict
+        db.close()
+        return redirect(url_for("inventory"))
+    else:
+        invDict = {}
+        db = shelve.open("inventory.db", "r")
+        invDict = db["inventory"]
+        db.close()
+        stock = invDict.get(id)
+        name = stock.get_name().split(" ", 1)[1]
+        updateStockInfoForm.productName.data = name
+        updateStockInfoForm.currentStock.data = stock.get_currentStock()
+        updateStockInfoForm.soldStock.data = stock.get_soldStock()
+        updateStockInfoForm.costPrice.data = stock.get_costPrice()
+        updateStockInfoForm.sellPrice.data = stock.get_sellingPrice()
+        return render_template("updateStockInfo.html", cookieDict=cookieDict, form=updateStockInfoForm)
+
+
+@app.route("/deleteStock/<int:id>", methods=["POST"])
+def deleteStock(id):
+    invDict = {}
+    db = shelve.open("inventory.db", "w")
+    invDict = db["inventory"]
+    invDict.pop(id)
+    db["inventory"] = invDict
+    db.close()
+    return redirect(url_for("inventory"))
 
 
 @app.route("/signup")
