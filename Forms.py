8999b4daba8110c1ac905703b22f5f9e410587df
BK201-sys <58057781+BK201-sys@users.noreply.github.com>
2020-01-13 19:33:09 +0800

    Update Forms.py

--- Forms.py
+++ Forms.py
@@ -1,19 +1,16 @@
-from wtforms import Form, StringField, SelectField, validators, FloatField, IntegerField, ValidationError, Field
+from wtforms import Form, StringField, SelectField, validators, FloatField, IntegerField, ValidationError
 
 
 class importForm(Form):
-
     brand = SelectField("Brand Name", [validators.DataRequired()], choices=[("","Select"), ("pilot","Pilot"), ("uni-ball", "Uni-ball")], default="")
-    productName = StringField("Product Name", [validators.DataRequired(), validators.Length(min=1, max=70)])
-    quantity = IntegerField("Quantity", [validators.DataRequired(), validators.NumberRange(min=1, max=200)])
+    productName = StringField("Product Name", [validators.DataRequired(), validators.Length(max=70)])
+    quantity = IntegerField("Quantity", [validators.DataRequired(), validators.NumberRange(max=1000)])
     costPrice = FloatField("Cost Price", [validators.DataRequired(), validators.NumberRange(min=0.01)])
     sellPrice = FloatField("Selling Price", [validators.DataRequired(), validators.NumberRange(min=0.01)])
 
 
 class updateForm(Form):
-
     productName = StringField("Product Name", [validators.DataRequired(), validators.Length(min=1, max=70)])
-    currentStock = IntegerField("Current Stock", [validators.DataRequired()])
-    soldStock = IntegerField("Sold Stock", [validators.DataRequired()])
+    currentStock = IntegerField("Current Stock", [validators.InputRequired()])
     costPrice = FloatField("Cost Price", [validators.DataRequired(), validators.NumberRange(min=0.01)])
     sellPrice = FloatField("Selling Price", [validators.DataRequired(), validators.NumberRange(min=0.01)])
